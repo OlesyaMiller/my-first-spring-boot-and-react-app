1a683bc1caf3deb57e4e5df845fbb377811f7505
olesya <olesyam.miller@gmail.com>
2022-03-31 13:44:04 -0400

    set up getters and setters for Student model

--- src/main/java/com/example/olesya/model/Student.java
+++ src/main/java/com/example/olesya/model/Student.java
@@ -1,5 +1,15 @@
 package com.example.olesya.model;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String address;
 }
