8a41ed16a3bff1ad52de664cea02b3efc7b48b99
olesya <olesyam.miller@gmail.com>
2022-03-31 13:47:53 -0400

    set up getters and setters for Student model

--- src/main/java/com/example/olesya/model/Student.java
+++ src/main/java/com/example/olesya/model/Student.java
@@ -12,4 +12,32 @@ public class Student {
     private int id;
     private String name;
     private String address;
+
+    public Student() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress(String address) {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
