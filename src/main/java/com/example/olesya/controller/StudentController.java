4fe5b77b101d1d2dd8ca6ec5bd13fe6620b7f858
olesya <olesyam.miller@gmail.com>
2022-03-31 11:56:04 -0400

    set up file structure for the project

---
+++ src/main/java/com/example/olesya/controller/StudentController.java
@@ -0,0 +1,5 @@
+package com.example.olesya.controller;
+
+public class StudentController {
+    
+}
---
+++ src/main/java/com/example/olesya/service/StudentService.java
@@ -0,0 +1,5 @@
+package com.example.olesya.service;
+
+public interface StudentService {
+    
+}
