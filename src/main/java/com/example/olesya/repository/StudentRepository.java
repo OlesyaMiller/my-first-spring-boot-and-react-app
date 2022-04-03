dac4ec77f9a2bfaae292aa3c5c5b804aaf3b139d
olesya <olesyam.miller@gmail.com>
2022-04-03 15:00:01 -0400

    extend StuddentRepository interface with JpaRepository, add imports and metadata

--- src/main/java/com/example/olesya/repository/StudentRepository.java
+++ src/main/java/com/example/olesya/repository/StudentRepository.java
@@ -1,5 +1,10 @@
 package com.example.olesya.repository;
 
-public interface StudentRepository {
+import com.example.olesya.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer>{
     
 }
