a092c2f364f95ae6b42b2cec901a44d5e15dfff5
olesya <olesyam.miller@gmail.com>
2022-04-14 17:05:27 -0400

    fix StudentServiceImp file

--- src/main/java/com/example/olesya/service/StudentServiceImpl.java
+++ src/main/java/com/example/olesya/service/StudentServiceImpl.java
@@ -2,10 +2,10 @@ package com.example.olesya.service;
 
 import com.example.olesya.model.Student;
 import com.example.olesya.repository.StudentRepository;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService  {
     
     @Autowired
